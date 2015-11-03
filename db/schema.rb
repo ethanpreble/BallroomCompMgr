383bd194c75f5546eb16872287a6fb2ce4d576f2
ethanpreble <ethanpreble@gmail.com>
2015-11-02 17:41:37 -0800

    adding first_name last_name to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030194814) do
+ActiveRecord::Schema.define(version: 20151103012051) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20151030194814) do
     t.datetime "locked_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name",             default: "", null: false
+    t.string   "last_name",              default: "", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
